b18103c8c7feb15b151190c8754d35a64bb4ab5b
Neha Ahire <neha.dani2011@gmail.com>
2023-04-15 01:10:32 +0530

    overridden Assignmant Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+public class Main {
+    public  void main(String[] args) {
+        A objA = new A();
+        System.out.println(objA.meth()); // prints "Invoking method from class A"
+
+        B objB = new B();
+        System.out.println(objB.meth()); // prints "Method is overridden in Extended class B"
+    }
 class A{
     public String meth(){
         return "Invoking method from class A";
@@ -11,12 +19,5 @@ class B extends A{
         return "Method is overridden in Extended class B";
     }
 }
-public class Main {
-    public static void main(String[] args) {
-        A objA = new A();
-        System.out.println(objA.meth()); // prints "Invoking method from class A"
 
-        B objB = new B();
-        System.out.println(objB.meth()); // prints "Method is overridden in Extended class B"
-    }
 }
